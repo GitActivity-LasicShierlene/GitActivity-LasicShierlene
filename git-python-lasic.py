aedbe4949a60174f12ee487a479683808b928e83
Shienie <127647298+GitActivity-LasicShierlene@users.noreply.github.com>
2023-03-12 15:32:26 +0800

    My Educational Background added

---
+++ git-python-lasic.py
@@ -0,0 +1,11 @@
+college = input ("Marinduque State College")
+print(college)
+
+course = input ("BS Information Technology")
+print(course)
+
+year = input ("3rd year")
+print(year)
+
+section = input("section B")
+print(section)
